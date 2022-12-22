18a0b2a96fdf7582628ebea9b293d2f916ce74bc
Steve Hunley <shunley2000@yahoo.com>
2022-12-22 06:53:16 -0600

    Added env variable for key

--- src/components/Search/api.ts
+++ src/components/Search/api.ts
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'd068f4f271mshcdf6fe441f040cfp1525c7jsna7cfd729dddd',
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY,
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 }

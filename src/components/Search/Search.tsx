231f81200f6b0a6cd4e8745c894a65c08c009563
Steve Hunley <shunley2000@yahoo.com>
2022-12-22 10:47:20 -0600

    Search WIP

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -7,6 +7,8 @@ export interface SearchProps {
   onSearchChange: (newValue: SingleValue<string>) => void
 }
 
+export const DFLT_COUNTRY_CODE = 'US'
+
 const Search = ({ onSearchChange }: SearchProps) => {
   const [search, setSearch] = useState<string | null>(null)
 
@@ -17,39 +19,68 @@ const Search = ({ onSearchChange }: SearchProps) => {
 
     if (!inputValue) return emptyOptions
 
-    const [namePrefix, regionCode, countryCode] = inputValue
+    const [namePrefix, regionCode, tmpCountryCode] = inputValue
       .split(',')
       .map((part) => part.toLowerCase())
       .map((part) => part.trim())
 
-    const results = fetch(
-      `${GEO_API_URL}/cities?limit=10&namePrefix=${namePrefix}`,
-      geoApiOptions
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response.data
-            .filter((city: any) => {
-              return regionCode
-                ? city.regionCode.toLowerCase() === regionCode
-                : true
-            })
-            .filter((city: any) => {
-              return countryCode
-                ? city.countryCode.toLowerCase() === countryCode
-                : true
-            })
-            .map((city: any) => {
+    // If regionCode has been specified, default countryCode
+    // to US if countryCode is not entered
+    const countryCode = regionCode
+      ? tmpCountryCode || DFLT_COUNTRY_CODE
+      : undefined
+
+    // https://wft-geo-db.p.rapidapi.com/v1/geo/countries/US/regions/CA/cities
+
+    let url
+    let results
+
+    if (namePrefix && regionCode && countryCode) {
+      url = `${GEO_API_URL}/countries/${countryCode}/regions/${regionCode}/cities?namePrefix=${namePrefix}`
+
+      results = fetch(url, geoApiOptions)
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data.map((city: any) => {
               return {
                 value: `${city.latitude} ${city.longitude}`,
-                label: `${city.name}, ${city.regionCode}, ${city.country}`,
+                label: `${city.name}, ${regionCode}, ${countryCode}`,
               }
             }),
-        }
-      })
-      // eslint-disable-next-line no-console
-      .catch((err) => console.error(err))
+          }
+        })
+        // eslint-disable-next-line no-console
+        .catch((err) => console.error(err))
+    } else {
+      url = `${GEO_API_URL}/cities?limit=10&namePrefix=${namePrefix}`
+
+      results = fetch(url, geoApiOptions)
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data
+              .filter((city: any) => {
+                return regionCode
+                  ? city.regionCode.toLowerCase() === regionCode
+                  : true
+              })
+              .filter((city: any) => {
+                return countryCode
+                  ? city.countryCode.toLowerCase() === countryCode
+                  : true
+              })
+              .map((city: any) => {
+                return {
+                  value: `${city.latitude} ${city.longitude}`,
+                  label: `${city.name}, ${city.regionCode}, ${city.countryCode}`,
+                }
+              }),
+          }
+        })
+        // eslint-disable-next-line no-console
+        .catch((err) => console.error(err))
+    }
 
     return results || emptyOptions
   }
